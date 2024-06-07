8e5c5eac042c6dac12b00429e987733a32b929e2
DiogenesHSCosta <diogenes.henriquecosta@outlook.com>
2024-06-07 16:12:05 -0300

    correção carrosel

--- src/js/script.js
+++ src/js/script.js
@@ -28,12 +28,7 @@ let ObjetivoRadio1 = document.querySelector("#objetivo-radio1")
 let ObjetivoRadio2 = document.querySelector("#objetivo-radio2")
 let ObjetivoRadio3 = document.querySelector("#objetivo-radio3")
 
-let naPraticaRadio1 = document.querySelector("#naPratica-radio1")
-let naPraticaRadio2 = document.querySelector("#naPratica-radio2")
-let naPraticaRadio3 = document.querySelector("#naPratica-radio3")
-
 let carroselObjetivo = document.querySelector(`.carrosel-objetivo`)
-let carroselNaPratica = document.querySelector(`.carrosel-naPratica`)
 
 function ObjetivoChecando_1(){
     if(ObjetivoRadio1.checked == true){        
@@ -51,29 +46,8 @@ function ObjetivoChecando_3(){
     }
 }
 
-
-
-function naPraticaChecando_1(){
-    if(naPraticaRadio1.checked == true){      
-        carroselNaPratica.style.transform = `translateX(-0vw)`
-    }
-}
-function naPraticaChecando_2(){
-    if(naPraticaRadio2.checked == true){
-        carroselNaPratica.style.transform = `translateX(-100vw)`
-    }
-}
-function naPraticaChecando_3(){
-    if(naPraticaRadio2.checked == true){        
-        carroselNaPratica.style.transform = `translateX(-200vw)`
-    }
-}
 setInterval(()=>{
     ObjetivoChecando_1()
     ObjetivoChecando_2()
     ObjetivoChecando_3()
-
-    naPraticaChecando_1()
-    naPraticaChecando_2()
-    naPraticaChecando_3()
 },100)
