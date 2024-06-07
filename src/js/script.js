6bb7074df44757afb8bb0bd7998483d2e178f97b
Pedro Camacho <pedroalmeidacamacho@gmail.com>
2024-06-07 11:05:33 -0300

    função logando input checked

--- src/js/script.js
+++ src/js/script.js
@@ -7,29 +7,54 @@ window.addEventListener('scroll', () =>{
     header.classList.toggle('animado', window.scrollY > 0)
 })
 
-// document.getElementById('radio-objetivo1').checked = true
+document.getElementById('radio-objetivo1').checked = true
+setInterval(() =>{
+    proximo()
+}, 5000)
 
-// setInterval(() =>{
-//     proximo()
-// }, 5000)
+function proximo(){
+    if (document.getElementById(`radio-objetivo1`).checked){
+        contProximo  = 2
+        visivel.style.marginLeft = '-32.8%';
+    }
+   
+    if (document.getElementById(`radio-objetivo2`).checked){
+        contProximo  = 3
+        visivel.style.marginLeft = '-65,6%';
+    }
 
-// function proximo(){
-//     if (document.getElementById(`radio-objetivo1`).checked){
-//         contProximo  = 2
-//         visivel.style.marginLeft = '-32.8%';
-//     }
-    
-//     if (document.getElementById(`radio-objetivo2`).checked){
-//         contProximo  = 3
-//         visivel.style.marginLeft = '-65,6%';
-//     }
+    if (document.getElementById(`radio-objetivo3`).checked){
+        contProximo  = 1
+        visivel.style.marginLeft = '0%';
+    }
+    console.log(contProximo, margin)
+    document.getElementById(`radio-objetivo${contProximo}`).checked = true
+}
+//tentado deixar selecionado
+/*
+let inputRadio1 = document.querySelector("#radio-objetivo1")
+let inputRadio2= document.querySelector("#radio-objetivo2")
+let inputRadio3 = document.querySelector("#radio-objetivo3")
 
-//     if (document.getElementById(`radio-objetivo3`).checked){
-//         contProximo  = 1
-//         visivel.style.marginLeft = '0%';
-//     }
-//     console.log(contProximo, margin)
+function checando_1(){
+    if(inputRadio1.checked == true){
+        console.log("logado 1")
+    }
+}
+function checando_2(){
+    if(inputRadio2.checked == true){
+        console.log("logado 2")
+    }
+}
+function checando_3(){
+    if(inputRadio3.checked == true){
+        console.log("logado 3")
+    }
+}
+setInterval(()=>{
+    checando_1()
+    checando_2()
+    checando_3()
+},100)
 
-
-//     document.getElementById(`radio-objetivo${contProximo}`).checked = true
-// }
+*/
