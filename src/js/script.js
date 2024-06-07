d25f953986e6a229764919e2160994d35a643bba
DiogenesHSCosta <diogenes.henriquecosta@outlook.com>
2024-06-07 14:43:48 -0300

    inserção caixas

--- src/js/script.js
+++ src/js/script.js
@@ -24,27 +24,27 @@ function mudarCarrosel(nome){
     carrosel.style.transform = `translateX(-${(cont - 1)*100}vw)`
 }
 
-let inputRadio1 = document.querySelector("#objetivo-radio1")
-let inputRadio2= document.querySelector("#objetivo-radio2")
-let inputRadio3 = document.querySelector("#objetivo-radio3")
+// let inputRadio1 = document.querySelector("#objetivo-radio1")
+// let inputRadio2= document.querySelector("#objetivo-radio2")
+// let inputRadio3 = document.querySelector("#objetivo-radio3")
 
-function checando_1(){
-    if(inputRadio1.checked == true){        
-        carrosel.style.transform = `translateX(-0vw)`
-    }
-}
-function checando_2(){
-    if(inputRadio2.checked == true){
-        carrosel.style.transform = `translateX(-100vw)`
-    }
-}
-function checando_3(){
-    if(inputRadio3.checked == true){        
-        carrosel.style.transform = `translateX(-200vw)`
-    }
-}
-setInterval(()=>{
-    checando_1()
-    checando_2()
-    checando_3()
-},100)
+// function checando_1(){
+//     if(inputRadio1.checked == true){        
+//         carrosel.style.transform = `translateX(-0vw)`
+//     }
+// }
+// function checando_2(){
+//     if(inputRadio2.checked == true){
+//         carrosel.style.transform = `translateX(-100vw)`
+//     }
+// }
+// function checando_3(){
+//     if(inputRadio3.checked == true){        
+//         carrosel.style.transform = `translateX(-200vw)`
+//     }
+// }
+// setInterval(()=>{
+//     checando_1()
+//     checando_2()
+//     checando_3()
+// },100)
