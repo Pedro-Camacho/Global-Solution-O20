0a026694f83be86a213c46645ea62095bc1ed13d
Pedro Camacho <pedroalmeidacamacho@gmail.com>
2024-06-07 12:39:21 -0300

    FuNCIONOU

--- src/js/script.js
+++ src/js/script.js
@@ -23,3 +23,29 @@ function mudarCarrosel(nome){
     document.getElementById(`${nome}-radio${cont}`).checked = true
     carrosel.style.transform = `translateX(-${(cont - 1)*100}vw)`
 }
+
+let inputRadio1 = document.querySelector("#objetivo-radio1")
+let inputRadio2= document.querySelector("#objetivo-radio2")
+let inputRadio3 = document.querySelector("#objetivo-radio3")
+
+function checando_1(){
+    if(inputRadio1.checked == true){        
+        carrosel.style.transform = `translateX(-0vw)`
+        console.log("passei aqui e não funcionei")
+    }
+}
+function checando_2(){
+    if(inputRadio2.checked == true){
+        carrosel.style.transform = `translateX(-100vw)`
+    }
+}
+function checando_3(){
+    if(inputRadio3.checked == true){        
+        carrosel.style.transform = `translateX(-200vw)`
+    }
+}
+setInterval(()=>{
+    checando_1()
+    checando_2()
+    checando_3()
+},100)
