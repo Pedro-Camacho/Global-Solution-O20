dc8686a3d1de18b9b21383544aa4d44aa512e58d
Pedro Camacho <pedroalmeidacamacho@gmail.com>
2024-06-07 11:14:25 -0300

    corrigindo interatividade

--- src/js/script.js
+++ src/js/script.js
@@ -2,15 +2,20 @@ let contProximo = 1
 var visivel = document.querySelector('.visivel')
 let margin = 0
 
+
+
+let inputRadio1 = document.querySelector("#radio-objetivo1")
+let inputRadio2= document.querySelector("#radio-objetivo2")
+let inputRadio3 = document.querySelector("#radio-objetivo3")
+
+//usúario escrola a tela e aparece o header animado
 window.addEventListener('scroll', () =>{
     const header = document.querySelector('header')
     header.classList.toggle('animado', window.scrollY > 0)
 })
 
+//faz o input radio 1 checkado com load
 document.getElementById('radio-objetivo1').checked = true
-setInterval(() =>{
-    proximo()
-}, 5000)
 
 function proximo(){
     if (document.getElementById(`radio-objetivo1`).checked){
@@ -31,24 +36,23 @@ function proximo(){
     document.getElementById(`radio-objetivo${contProximo}`).checked = true
 }
 //tentado deixar selecionado
-/*
-let inputRadio1 = document.querySelector("#radio-objetivo1")
-let inputRadio2= document.querySelector("#radio-objetivo2")
-let inputRadio3 = document.querySelector("#radio-objetivo3")
 
 function checando_1(){
-    if(inputRadio1.checked == true){
-        console.log("logado 1")
+    if(inputRadio1.checked == true){        
+        contProximo = 2
+        visivel.style.marginLeft = '0%';        
     }
 }
 function checando_2(){
     if(inputRadio2.checked == true){
-        console.log("logado 2")
+        contProximo = 3
+        visivel.style.marginLeft = '-32.8%';
     }
 }
 function checando_3(){
     if(inputRadio3.checked == true){
-        console.log("logado 3")
+        contProximo = 1
+        visivel.style.marginLeft = '-65,6%';
     }
 }
 setInterval(()=>{
@@ -57,4 +61,8 @@ setInterval(()=>{
     checando_3()
 },100)
 
-*/
+//gira o carrossel a cada 5 segundos
+setInterval(() =>{
+    proximo()
+}, 5000)
+
